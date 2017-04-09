b46dc8499bccd27ab14f0734cca4889e6cbe8838
yuhanlyu <yuhanlyu@gmail.com>
2017-04-09 16:15:52 -0700

    Add example

--- number-theory/primality/sieve/linear/linear_sieve.h
+++ number-theory/primality/sieve/linear/linear_sieve.h
@@ -6,8 +6,18 @@
 #include <cstdint>
 
 // Pritchard's linear sieve method.
+// Example usage:
+// constexpr int32_t size = 1 << 20;
+// bool prime[size / 2];
+// LinearSieve(size, prime);
+// IsPrime(n, prime);
 void LinearSieve(uint32_t n, bool prime[]);
 
+// Example usage:
+// constexpr int32_t size = 1 << 20;
+// uint32_t prime[size / 2];
+// LinearSieveBit(size, prime);
+// IsPrimeBit(n, prime);
 void LinearSieveBit(uint64_t n, uint32_t prime[]);
 
 #endif
