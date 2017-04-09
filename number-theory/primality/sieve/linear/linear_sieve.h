f2ab6f73aa54a2a7bcef9e8afe2be9f698554a4a
yuhanlyu <yuhanlyu@gmail.com>
2017-04-09 16:17:26 -0700

    Add example

--- number-theory/primality/sieve/linear/linear_sieve.h
+++ number-theory/primality/sieve/linear/linear_sieve.h
@@ -15,7 +15,7 @@ void LinearSieve(uint32_t n, bool prime[]);
 
 // Example usage:
 // constexpr int32_t size = 1 << 20;
-// uint32_t prime[size / 2];
+// uint32_t prime[size / 64 + 1];
 // LinearSieveBit(size, prime);
 // IsPrimeBit(n, prime);
 void LinearSieveBit(uint64_t n, uint32_t prime[]);
--- number-theory/primality/sieve/segmented/segmented_sieve.h
+++ number-theory/primality/sieve/segmented/segmented_sieve.h
@@ -6,8 +6,18 @@
 #include "../common/common.h"
 
 // Segmented sieve method.
+// Example usage:
+// constexpr int32_t size = 1 << 20;
+// bool prime[size / 2];
+// SegmentedSieve(size, prime);
+// IsPrime(n, prime);
 void SegmentedSieve(uint32_t n, bool prime[]);
 
+// Example usage:
+// constexpr int32_t size = 1 << 20;
+// uint32_t prime[size / 64 + 1];
+// SegmentedSieveBit(size, prime);
+// IsPrimeBit(n, prime);
 void SegmentedSieveBit(uint64_t n, uint32_t prime[]);
 
 #endif
