9402e3c704f91636beb72c9b50c2839c99ec2bbe
yuhanlyu <yuhanlyu@gmail.com>
2017-08-06 10:31:59 -0700

    Remove template/macro

--- data-structures/ordered-set/weight-balanced-tree/README.md
+++ data-structures/ordered-set/weight-balanced-tree/README.md
@@ -33,26 +33,6 @@ WBTBenchmark/WBTInsert/65536     11878268 ns   11876806 ns         59
 WBTBenchmark/WBTInsert/262144    73615756 ns   73609235 ns         10
 WBTBenchmark/WBTInsert/1048576  543782022 ns  543738739 ns          1
 
-------------------------------------------------------------------------------
-Benchmark                                       Time           CPU Iterations
-------------------------------------------------------------------------------
-WBTBenchmark/WBTInsertTemplate/1024         76654 ns      76668 ns       9061
-WBTBenchmark/WBTInsertTemplate/4096        361854 ns     361832 ns       1922
-WBTBenchmark/WBTInsertTemplate/16384      2176727 ns    2176581 ns        319
-WBTBenchmark/WBTInsertTemplate/65536     12361515 ns   12360540 ns         56
-WBTBenchmark/WBTInsertTemplate/262144    76471641 ns   76466009 ns         10
-WBTBenchmark/WBTInsertTemplate/1048576  561743381 ns  561701425 ns          1
-
----------------------------------------------------------------------------
-Benchmark                                    Time           CPU Iterations
----------------------------------------------------------------------------
-WBTBenchmark/WBTInsertMacro/1024         72609 ns      72617 ns       9518
-WBTBenchmark/WBTInsertMacro/4096        346877 ns     346861 ns       2039
-WBTBenchmark/WBTInsertMacro/16384      2091063 ns    2090932 ns        338
-WBTBenchmark/WBTInsertMacro/65536     11763907 ns   11763017 ns         58
-WBTBenchmark/WBTInsertMacro/262144    69662152 ns   69656972 ns         10
-WBTBenchmark/WBTInsertMacro/1048576  521871706 ns  521833763 ns          1
-
 ----------------------------------------------------------------------
 Benchmark                               Time           CPU Iterations
 ----------------------------------------------------------------------
@@ -62,25 +42,4 @@ WBTBenchmark/WBTDelete/16384      1916533 ns    1916334 ns        369
 WBTBenchmark/WBTDelete/65536     10835475 ns   10834715 ns         65
 WBTBenchmark/WBTDelete/262144    65703348 ns   65698357 ns         11
 WBTBenchmark/WBTDelete/1048576  506542678 ns  506508165 ns          1
-
-------------------------------------------------------------------------------
-Benchmark                                       Time           CPU Iterations
-------------------------------------------------------------------------------
-WBTBenchmark/WBTDeleteTemplate/1024         61564 ns      61557 ns      11471
-WBTBenchmark/WBTDeleteTemplate/4096        306246 ns     306225 ns       2328
-WBTBenchmark/WBTDeleteTemplate/16384      1847573 ns    1847437 ns        381
-WBTBenchmark/WBTDeleteTemplate/65536     10525370 ns   10524673 ns         62
-WBTBenchmark/WBTDeleteTemplate/262144    61923990 ns   61919262 ns         12
-WBTBenchmark/WBTDeleteTemplate/1048576  524983824 ns  524947602 ns          2
-
----------------------------------------------------------------------------
-Benchmark                                    Time           CPU Iterations
----------------------------------------------------------------------------
-WBTBenchmark/WBTDeleteMacro/1024         61102 ns      61097 ns      11531
-WBTBenchmark/WBTDeleteMacro/4096        302223 ns     302196 ns       2331
-WBTBenchmark/WBTDeleteMacro/16384      1848850 ns    1848671 ns        380
-WBTBenchmark/WBTDeleteMacro/65536     10323358 ns   10322650 ns         67
-WBTBenchmark/WBTDeleteMacro/262144    62380202 ns   62376277 ns         12
-WBTBenchmark/WBTDeleteMacro/1048576  515080116 ns  515048557 ns          2
-
 </pre>
--- data-structures/ordered-set/weight-balanced-tree/weight_balanced_tree_test.cc
+++ data-structures/ordered-set/weight-balanced-tree/weight_balanced_tree_test.cc
@@ -13,7 +13,7 @@ using testing::ElementsAreArray;
 
 constexpr int max_test_size = 7;
 
-TEST(WeightBalancedTree, InsertAndRank) {
+TEST(WeightBalancedTree, Insert) {
   for (int size = 1; size <= max_test_size; ++size) {
     std::vector<int> expected_result;
     for (int i = 0; i < size; ++i) expected_result.push_back(i + 1);
@@ -33,7 +33,7 @@ TEST(WeightBalancedTree, InsertAndRank) {
   }
 }
 
-TEST(WeightBalancedTree, DeleteAndRank) {
+TEST(WeightBalancedTree, Delete) {
   for (int size = 1; size <= max_test_size; ++size) {
     std::vector<int> inserted_elements;
     for (int i = 0; i < size; ++i) inserted_elements.push_back(i + 1);
@@ -42,90 +42,6 @@ TEST(WeightBalancedTree, DeleteAndRank) {
       WeightBalancedTree<int> tree;
       for (int value : temp) tree.Insert(value);
       tree.Delete(temp[0]);
-      EXPECT_TRUE(tree.IsBalanced());
-      std::vector<int> expected_result(temp);
-      expected_result.erase(expected_result.begin());
-      std::sort(expected_result.begin(), expected_result.end());
-      std::vector<int> actual_result;
-      tree.InorderTraverse(actual_result);
-      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
-      for (int i = 0; i < expected_result.size(); ++i)
-        EXPECT_EQ(i, tree.rank(expected_result[i]));
-    } while (std::next_permutation(temp.begin(), temp.end()));
-  }
-}
-
-TEST(WeightBalancedTree, InsertTemplateAndRank) {
-  for (int size = 1; size <= max_test_size; ++size) {
-    std::vector<int> expected_result;
-    for (int i = 0; i < size; ++i) expected_result.push_back(i + 1);
-    std::vector<int> inserted_elements(expected_result);
-    do {
-      WeightBalancedTree<int> tree;
-      for (int value : inserted_elements) {
-        tree.InsertTemplate(value);
-        EXPECT_TRUE(tree.IsBalanced());
-      }
-      std::vector<int> actual_result;
-      tree.InorderTraverse(actual_result);
-      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
-      for (int i = 0; i < size; ++i) EXPECT_EQ(i, tree.rank(i + 1));
-    } while (std::next_permutation(inserted_elements.begin(),
-                                   inserted_elements.end()));
-  }
-}
-
-TEST(WeightBalancedTree, DeleteTemplateAndRank) {
-  for (int size = 1; size <= max_test_size; ++size) {
-    std::vector<int> inserted_elements;
-    for (int i = 0; i < size; ++i) inserted_elements.push_back(i + 1);
-    std::vector<int> temp(inserted_elements);
-    do {
-      WeightBalancedTree<int> tree;
-      for (int value : temp) tree.InsertTemplate(value);
-      tree.DeleteTemplate(temp[0]);
-      EXPECT_TRUE(tree.IsBalanced());
-      std::vector<int> expected_result(temp);
-      expected_result.erase(expected_result.begin());
-      std::sort(expected_result.begin(), expected_result.end());
-      std::vector<int> actual_result;
-      tree.InorderTraverse(actual_result);
-      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
-      for (int i = 0; i < expected_result.size(); ++i)
-        EXPECT_EQ(i, tree.rank(expected_result[i]));
-    } while (std::next_permutation(temp.begin(), temp.end()));
-  }
-}
-
-TEST(WeightBalancedTree, InsertMacroAndRank) {
-  for (int size = 1; size <= max_test_size; ++size) {
-    std::vector<int> expected_result;
-    for (int i = 0; i < size; ++i) expected_result.push_back(i + 1);
-    std::vector<int> inserted_elements(expected_result);
-    do {
-      WeightBalancedTree<int> tree;
-      for (int value : inserted_elements) {
-        tree.InsertMacro(value);
-        EXPECT_TRUE(tree.IsBalanced());
-      }
-      std::vector<int> actual_result;
-      tree.InorderTraverse(actual_result);
-      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
-      for (int i = 0; i < size; ++i) EXPECT_EQ(i, tree.rank(i + 1));
-    } while (std::next_permutation(inserted_elements.begin(),
-                                   inserted_elements.end()));
-  }
-}
-
-TEST(WeightBalancedTree, DeleteMacroAndRank) {
-  for (int size = 1; size <= max_test_size; ++size) {
-    std::vector<int> inserted_elements;
-    for (int i = 0; i < size; ++i) inserted_elements.push_back(i + 1);
-    std::vector<int> temp(inserted_elements);
-    do {
-      WeightBalancedTree<int> tree;
-      for (int value : temp) tree.InsertMacro(value);
-      tree.DeleteMacro(temp[0]);
       EXPECT_TRUE(tree.IsBalanced());
       std::vector<int> expected_result(temp);
       expected_result.erase(expected_result.begin());
