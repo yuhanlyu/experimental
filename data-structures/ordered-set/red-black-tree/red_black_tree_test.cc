8539774b763563a958ff5619f8d1d19255bb6b7a
yuhanlyu <yuhanlyu@gmail.com>
2017-08-27 15:06:26 -0700

    More Tests

--- data-structures/ordered-set/red-black-tree/red_black_tree_test.cc
+++ data-structures/ordered-set/red-black-tree/red_black_tree_test.cc
@@ -1,6 +1,9 @@
 #include "red_black_tree.h"
 
 #include <algorithm>
+#include <random>
+#include <set>
+#include <unordered_map>
 #include <vector>
 
 #include "../binary-search-tree-common/binary_search_tree_common.h"
@@ -29,11 +32,40 @@ TEST(RBTree, Insert) {
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
       for (int value : inserted_elements) {
         EXPECT_FALSE(tree.Insert(value));
+        actual_result.clear();
+        tree.InorderTraverse(actual_result);
         EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
         EXPECT_TRUE(tree.IsBalanced());
       }
     } while (std::next_permutation(inserted_elements.begin(),
                                    inserted_elements.end()));
+  }
+}
+
+TEST(RBTree, InsertRandom) {
+  int sizes[] = {10, 100, 200, 500, 1000, 2000};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> expected_result;
+    for (int i = 0; i < size; ++i) expected_result.push_back(i + 1);
+    std::vector<int> inserted_elements(expected_result);
+    std::shuffle(inserted_elements.begin(), inserted_elements.end(), g);
+    RBTree<int> tree;
+    for (int value : inserted_elements) {
+      EXPECT_TRUE(tree.Insert(value));
+      EXPECT_TRUE(tree.IsBalanced());
+    }
+    std::vector<int> actual_result;
+    tree.InorderTraverse(actual_result);
+    EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    for (int value : inserted_elements) {
+      EXPECT_FALSE(tree.Insert(value));
+      EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
   }
 }
 
@@ -53,11 +85,40 @@ TEST(RBTree, BottomUpInsert) {
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
       for (int value : inserted_elements) {
         EXPECT_FALSE(tree.BottomUpInsert(value));
+        actual_result.clear();
+        tree.InorderTraverse(actual_result);
         EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
         EXPECT_TRUE(tree.IsBalanced());
       }
     } while (std::next_permutation(inserted_elements.begin(),
                                    inserted_elements.end()));
+  }
+}
+
+TEST(RBTree, BottomUpInsertRandom) {
+  int sizes[] = {10, 100, 200, 500, 1000, 2000};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> expected_result;
+    for (int i = 0; i < size; ++i) expected_result.push_back(i + 1);
+    std::vector<int> inserted_elements(expected_result);
+    std::shuffle(inserted_elements.begin(), inserted_elements.end(), g);
+    RBTree<int> tree;
+    for (int value : inserted_elements) {
+      EXPECT_TRUE(tree.BottomUpInsert(value));
+      EXPECT_TRUE(tree.IsBalanced());
+    }
+    std::vector<int> actual_result;
+    tree.InorderTraverse(actual_result);
+    EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    for (int value : inserted_elements) {
+      EXPECT_FALSE(tree.BottomUpInsert(value));
+      EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
   }
 }
 
@@ -77,11 +138,40 @@ TEST(RBTree, TopDownInsert) {
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
       for (int value : inserted_elements) {
         EXPECT_FALSE(tree.TopDownInsert(value));
+        actual_result.clear();
+        tree.InorderTraverse(actual_result);
         EXPECT_TRUE(tree.IsBalanced());
         EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
       }
     } while (std::next_permutation(inserted_elements.begin(),
                                    inserted_elements.end()));
+  }
+}
+
+TEST(RBTree, TopDownInsertRandom) {
+  int sizes[] = {10, 100, 200, 500, 1000, 2000};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> expected_result;
+    for (int i = 0; i < size; ++i) expected_result.push_back(i + 1);
+    std::vector<int> inserted_elements(expected_result);
+    std::shuffle(inserted_elements.begin(), inserted_elements.end(), g);
+    RBTree<int> tree;
+    for (int value : inserted_elements) {
+      EXPECT_TRUE(tree.TopDownInsert(value));
+      EXPECT_TRUE(tree.IsBalanced());
+    }
+    std::vector<int> actual_result;
+    tree.InorderTraverse(actual_result);
+    EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    for (int value : inserted_elements) {
+      EXPECT_FALSE(tree.TopDownInsert(value));
+      EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
   }
 }
 
@@ -103,8 +193,41 @@ TEST(RBTree, Delete) {
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
       EXPECT_FALSE(tree.Delete(temp[0]));
       EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
     } while (std::next_permutation(temp.begin(), temp.end()));
+  }
+}
+
+TEST(RBTree, DeleteRandom) {
+  int sizes[] = {10, 100, 200, 500, 1000, 2000, 5000};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> inserted_elements;
+    for (int i = 0; i < size; ++i) inserted_elements.push_back(i + 1);
+    std::shuffle(inserted_elements.begin(), inserted_elements.end(), g);
+    std::vector<int> temp(inserted_elements);
+    RBTree<int> tree;
+    for (int value : inserted_elements) ASSERT_TRUE(tree.Insert(value));
+    for (int i = 0; i < size / 10; ++i) {
+      EXPECT_TRUE(tree.Delete(inserted_elements[i]));
+      EXPECT_TRUE(tree.IsBalanced());
+    }
+    temp.erase(temp.begin(), temp.begin() + size / 10);
+    std::vector<int> expected_result(temp);
+    std::sort(expected_result.begin(), expected_result.end());
+    std::vector<int> actual_result;
+    tree.InorderTraverse(actual_result);
+    EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    for (int i = 0; i < size / 10; ++i) {
+      EXPECT_FALSE(tree.Delete(inserted_elements[i]));
+      EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
   }
 }
 
@@ -126,8 +249,41 @@ TEST(RBTree, BottomUpDelete) {
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
       EXPECT_FALSE(tree.BottomUpDelete(temp[0]));
       EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
     } while (std::next_permutation(temp.begin(), temp.end()));
+  }
+}
+
+TEST(RBTree, BottomUpDeleteRandom) {
+  int sizes[] = {10, 100, 200, 500, 1000, 2000, 5000};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> inserted_elements;
+    for (int i = 0; i < size; ++i) inserted_elements.push_back(i + 1);
+    std::shuffle(inserted_elements.begin(), inserted_elements.end(), g);
+    std::vector<int> temp(inserted_elements);
+    RBTree<int> tree;
+    for (int value : inserted_elements) ASSERT_TRUE(tree.Insert(value));
+    for (int i = 0; i < size / 10; ++i) {
+      EXPECT_TRUE(tree.BottomUpDelete(inserted_elements[i]));
+      EXPECT_TRUE(tree.IsBalanced());
+    }
+    temp.erase(temp.begin(), temp.begin() + size / 10);
+    std::vector<int> expected_result(temp);
+    std::sort(expected_result.begin(), expected_result.end());
+    std::vector<int> actual_result;
+    tree.InorderTraverse(actual_result);
+    EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    for (int i = 0; i < size / 10; ++i) {
+      EXPECT_FALSE(tree.BottomUpDelete(inserted_elements[i]));
+      EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
   }
 }
 
@@ -149,8 +305,155 @@ TEST(RBTree, TopDownDelete) {
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
       EXPECT_FALSE(tree.TopDownDelete(temp[0]));
       EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
       EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
     } while (std::next_permutation(temp.begin(), temp.end()));
+  }
+}
+
+TEST(RBTree, TopDownDeleteRandom) {
+  int sizes[] = {10, 100, 200, 500, 1000, 2000, 5000};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> inserted_elements;
+    for (int i = 0; i < size; ++i) inserted_elements.push_back(i + 1);
+    std::shuffle(inserted_elements.begin(), inserted_elements.end(), g);
+    std::vector<int> temp(inserted_elements);
+    RBTree<int> tree;
+    for (int value : inserted_elements) ASSERT_TRUE(tree.Insert(value));
+    for (int i = 0; i < size / 10; ++i) {
+      EXPECT_TRUE(tree.TopDownDelete(inserted_elements[i]));
+      EXPECT_TRUE(tree.IsBalanced());
+    }
+    temp.erase(temp.begin(), temp.begin() + size / 10);
+    std::vector<int> expected_result(temp);
+    std::sort(expected_result.begin(), expected_result.end());
+    std::vector<int> actual_result;
+    tree.InorderTraverse(actual_result);
+    EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    for (int i = 0; i < size / 10; ++i) {
+      EXPECT_FALSE(tree.TopDownDelete(inserted_elements[i]));
+      EXPECT_TRUE(tree.IsBalanced());
+      actual_result.clear();
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
+  }
+}
+
+TEST(RBTree, Mix) {
+  int sizes[] = {10, 100, 200, 500};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> elements;
+    for (int i = 0; i < size; ++i) elements.insert(elements.end(), 4, i + 1);
+    std::shuffle(elements.begin(), elements.end(), g);
+    RBTree<int> tree;
+    std::set<int> expected_tree;
+    std::unordered_map<int, int> count;
+    for (int value : elements) {
+      switch (++count[value]) {
+        case 1:
+          expected_tree.insert(value);
+          EXPECT_TRUE(tree.Insert(value));
+          break;
+        case 2:
+          EXPECT_FALSE(tree.Insert(value));
+          break;
+        case 3:
+          expected_tree.erase(value);
+          EXPECT_TRUE(tree.Delete(value));
+          break;
+        case 4:
+          EXPECT_FALSE(tree.Delete(value));
+          break;
+      }
+      EXPECT_TRUE(tree.IsBalanced());
+      std::vector<int> expected_result(expected_tree.begin(),
+                                       expected_tree.end());
+      std::vector<int> actual_result;
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
+  }
+}
+
+TEST(RBTree, BottomUpMix) {
+  int sizes[] = {10, 100, 200, 500};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> elements;
+    for (int i = 0; i < size; ++i) elements.insert(elements.end(), 4, i + 1);
+    std::shuffle(elements.begin(), elements.end(), g);
+    RBTree<int> tree;
+    std::set<int> expected_tree;
+    std::unordered_map<int, int> count;
+    for (int value : elements) {
+      switch (++count[value]) {
+        case 1:
+          expected_tree.insert(value);
+          EXPECT_TRUE(tree.BottomUpInsert(value));
+          break;
+        case 2:
+          EXPECT_FALSE(tree.BottomUpInsert(value));
+          break;
+        case 3:
+          expected_tree.erase(value);
+          EXPECT_TRUE(tree.BottomUpDelete(value));
+          break;
+        case 4:
+          EXPECT_FALSE(tree.BottomUpDelete(value));
+          break;
+      }
+      EXPECT_TRUE(tree.IsBalanced());
+      std::vector<int> expected_result(expected_tree.begin(),
+                                       expected_tree.end());
+      std::vector<int> actual_result;
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
+  }
+}
+
+TEST(RBTree, TopDownMix) {
+  int sizes[] = {10, 100, 200, 500};
+  for (int size : sizes) {
+    std::random_device rd;
+    std::mt19937 g(rd());
+    std::vector<int> elements;
+    for (int i = 0; i < size; ++i) elements.insert(elements.end(), 4, i + 1);
+    std::shuffle(elements.begin(), elements.end(), g);
+    RBTree<int> tree;
+    std::set<int> expected_tree;
+    std::unordered_map<int, int> count;
+    for (int value : elements) {
+      switch (++count[value]) {
+        case 1:
+          expected_tree.insert(value);
+          EXPECT_TRUE(tree.TopDownInsert(value));
+          break;
+        case 2:
+          EXPECT_FALSE(tree.TopDownInsert(value));
+          break;
+        case 3:
+          expected_tree.erase(value);
+          EXPECT_TRUE(tree.TopDownDelete(value));
+          break;
+        case 4:
+          EXPECT_FALSE(tree.TopDownDelete(value));
+          break;
+      }
+      EXPECT_TRUE(tree.IsBalanced());
+      std::vector<int> expected_result(expected_tree.begin(),
+                                       expected_tree.end());
+      std::vector<int> actual_result;
+      tree.InorderTraverse(actual_result);
+      EXPECT_THAT(actual_result, ElementsAreArray(expected_result));
+    }
   }
 }
 
