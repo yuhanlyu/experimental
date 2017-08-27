1fb1853c4440afa1cf65fb3b3affd5ac7a2a5bf6
yuhanlyu <yuhanlyu@gmail.com>
2017-08-27 15:18:56 -0700

    Refine

--- data-structures/ordered-set/red-black-tree/red_black_tree.h
+++ data-structures/ordered-set/red-black-tree/red_black_tree.h
@@ -12,176 +12,23 @@ struct RBTree {
     using value_type = T;
     explicit Node(const T& x, bool is_red = true) : value(x), red(is_red) {}
     Node() = default;
-    Node* left = sentinel;
-    Node* right = sentinel;
+    union {
+      Node* link[2] = {sentinel, sentinel};
+      struct {
+        Node* left;
+        Node* right;
+      };
+    };
     T value;
     bool red = false;
   };
 
-  bool Insert(const T& x) {
-    if (root_ == sentinel) {
-      root_ = new Node(x, false);
-      return true;
-    }
-    if (root_->left->red && root_->right->red)
-      root_->left->red = root_->right->red = false;
-    Node **parent = &root_, **safe_node = &root_;
-    // Find the lowest black node that has one black child.
-    while ((*parent) != sentinel) {
-      Node*& current = *parent;
-      if (x == current->value) return false;
-      // Use bitwise or is faster than the logical or.
-      if (!current->red && (!current->left->red | !current->right->red))
-        safe_node = parent;
-      parent = x < current->value ? &current->left : &current->right;
-    }
-    *parent = new Node(x);
-    Node *&node = *safe_node,
-         *next = x < node->value ? node->left : node->right, *current = next;
-    if (next == *parent) return true;
-    bool need_rotation = next->red;
-    if (need_rotation) current = x < next->value ? next->left : next->right;
-    // Flip color from the current to parent.
-    while (current != *parent) {
-      current->red = true;
-      current->left->red = current->right->red = false;
-      current = x < current->value ? current->left : current->right;
-      current = x < current->value ? current->left : current->right;
-    }
-    if (!need_rotation) return true;
-    // At this point, safe_node is black, next is red, next's sibling is
-    // black, and grand_child is red.
-    node->red = true;
-    if (x < node->value)
-      x < next->value ? RightRotate(node) : LRRotate(node);
-    else
-      x < next->value ? RLRotate(node) : LeftRotate(node);
-    node->red = false;
-    return true;
-  }
+  bool Insert(const T& x) { return Insert(root_, x); }
 
   bool Delete(const T& x) {
     bool deleted = Delete(root_, x);
     root_->red = false;
     return deleted;
-  }
-
-  bool TopDownInsert(const T& x) {
-    if (root_ == sentinel) {
-      root_ = new Node(x, false);
-      return true;
-    }
-    if (x == root_->value) return false;
-    for (Node *nil = &dummy, **gg_parent = &nil, **g_parent = &nil,
-              **parent = &root_;
-         ;) {
-      // Loop invariant: the sibling of parent (pointed by g_parent) is black.
-      // current is the node points by the parent, but after the rotation,
-      // parent might point to other node. But the search path is determined by
-      // the current.
-      Node* current = *parent;
-      if (current == sentinel)
-        current = *parent = new Node(x);
-      else if (x == current->value)
-        return root_->red = false;
-      else if (current->left->red && current->right->red) {
-        // Flip color.
-        current->red = true;
-        current->left->red = current->right->red = false;
-      }
-      // When two red nodes are connected.
-      if (current->red && (*g_parent)->red) {
-        (*gg_parent)->red = true;
-        if (x < (*gg_parent)->value)
-          x > (*g_parent)->value ? LRRotate(*gg_parent)
-                                 : RightRotate(*gg_parent);
-        else
-          x < (*g_parent)->value ? RLRotate(*gg_parent)
-                                 : LeftRotate(*gg_parent);
-        (*gg_parent)->red = false;
-        // When rotation occurs, rotation cannot occurs in the next iteration.
-        // 	 After double rotation, since current becomes black, red nodes
-        // 	 violation won't occur in the next iteration.
-        // 	 After single rotation, although current is red, since the
-        // children
-        // 	 and grand children must be black, red nodes violation won't
-        // occur
-        // 	 in the next iteration.
-        // It is safe to set parent to be nil, since when double rotation
-        // occurs, we loss the track of g_parent and gg_parent.
-        parent = &nil;
-      }
-      if (x == current->value) break;
-      gg_parent = g_parent;
-      g_parent = parent;
-      parent = x < current->value ? &current->left : &current->right;
-    }
-    root_->red = false;
-    return true;
-  }
-
-  bool TopDownDelete(const T& x) {
-    if (root_ == sentinel) return false;
-    // Set sibling and g_parent to nil pointer to simplify the initial
-    // condition.
-    Node *to_be_deleted = sentinel, nil, *sibling = &nil, **g_parent = &sibling;
-    for (Node **parent = &root_, **next, **next_sibling; *parent != sentinel;
-         g_parent = parent, parent = next, sibling = *next_sibling) {
-      // Loop invariant: when current is black, the sibling is black, and
-      // the parent node (pointed by g_parent) is red.
-      Node*& current = *parent;
-      if (x == current->value) to_be_deleted = current;
-      if (x < current->value) {
-        next = &current->left;
-        next_sibling = &current->right;
-      } else {
-        next = &current->right;
-        next_sibling = &current->left;
-      }
-      // Rebalance is required only when the current and the next node are
-      // black.
-      if (current->red || (*next)->red) continue;
-      // When next sibling is red, rotate.
-      if ((*next_sibling)->red) {
-        current->red = true;
-        (*next_sibling)->red = false;
-        if (x < current->value) {
-          LeftRotate(*parent);
-          parent = &(*parent)->left;
-        } else {
-          RightRotate(*parent);
-          parent = &(*parent)->right;
-        }
-        continue;
-      }
-      // Current has two black children.
-      // If sibling has two black children, flip the color.
-      if (!sibling->right->red && !sibling->left->red) {
-        (*g_parent)->red = false;
-        current->red = sibling->red = true;
-        continue;
-      }
-      // If sibling has one red child, rebalance base on whether the
-      // current node is left or right of its parent.
-      if (*parent == (*g_parent)->left) {
-        sibling->right->red ? LeftRotate(*g_parent) : RLRotate(*g_parent);
-        parent = &(*g_parent)->left->left;
-      } else {
-        sibling->left->red ? RightRotate(*g_parent) : LRRotate(*g_parent);
-        parent = &(*g_parent)->right->right;
-      }
-      current->red = (*g_parent)->red = true;
-      (*g_parent)->left->red = (*g_parent)->right->red = false;
-    }
-    if (to_be_deleted == sentinel) return root_->red = false;
-    // When the loop terminates, (*g_parent)->value is the successor of x.
-    to_be_deleted->value = (*g_parent)->value;
-    to_be_deleted = *g_parent;
-    *g_parent =
-        (*g_parent)->left == sentinel ? (*g_parent)->right : (*g_parent)->left;
-    delete to_be_deleted;
-    root_->red = false;
-    return true;
   }
 
   bool BottomUpInsert(const T& x) {
@@ -203,6 +50,10 @@ struct RBTree {
     if (root_->red) return false;
     return BlackHeight(root_) != -1;
   }
+
+  bool TopDownInsert(const T& x) { return TopDownInsert(root_, x); }
+
+  bool TopDownDelete(const T& x) { return TopDownDelete(root_, x); }
 
   ~RBTree() { FreeTree(root_, sentinel); }
 
@@ -233,13 +84,57 @@ struct RBTree {
     return lh + (node->red ? 0 : 1);
   }
 
-  static bool Delete(Node*& root, const T& x) {
-    Node **parent = &root, **safe_node = &root, *current;
-    if (!root->red && !root->left->red && !root->right->red) root->red = true;
+  static bool Insert(Node*& node, const T& x) {
+    if (node == sentinel) {
+      node = new Node(x, false);
+      return true;
+    }
+    if (node->left->red && node->right->red)
+      node->left->red = node->right->red = false;
+    Node **parent = &node, **safe_node = &node;
+    // Find the lowest black node that has one black child.
+    while ((*parent) != sentinel) {
+      Node*& current = *parent;
+      if (x == current->value) return false;
+      // Use bitwise or is faster than the logical or.
+      if (!current->red && (!current->left->red | !current->right->red))
+        safe_node = parent;
+      parent = x < current->value ? &current->left : &current->right;
+    }
+    *parent = new Node(x);
+    Node *&update_node = *safe_node,
+         *next =
+             x < update_node->value ? update_node->left : update_node->right,
+         *current = next;
+    if (next == *parent) return true;
+    bool need_rotation = next->red;
+    if (need_rotation) current = x < next->value ? next->left : next->right;
+    // Flip color from the current to parent.
+    while (current != *parent) {
+      current->red = true;
+      current->left->red = current->right->red = false;
+      current = x < current->value ? current->left : current->right;
+      current = x < current->value ? current->left : current->right;
+    }
+    if (!need_rotation) return true;
+    // At this point, safe_node is black, next is red, next's sibling is
+    // black, and grand_child is red.
+    update_node->red = true;
+    if (x < update_node->value)
+      x < next->value ? RightRotate(update_node) : LRRotate(update_node);
+    else
+      x < next->value ? RLRotate(update_node) : LeftRotate(update_node);
+    update_node->red = false;
+    return true;
+  }
+
+  static bool Delete(Node*& node, const T& x) {
+    Node **parent = &node, **safe_node = &node, *current;
+    if (!node->red && !node->left->red && !node->right->red) node->red = true;
     const T* delete_value = &x;
     // Find the node to be deleted and the lowest red node or black node with
     // red child/grandchild.
-    for (current = root; current != sentinel; current = *parent) {
+    for (current = node; current != sentinel; current = *parent) {
       // Using bitwise or is faster than logical or.
       if (current->red | current->left->red | current->right->red |
           current->left->left->red | current->left->right->red |
@@ -318,14 +213,14 @@ struct RBTree {
     }
     // At this point, both next and sibling are black, and one of sibling's
     // child is red.
-    Node*& node = *safe_node;
-    bool old_node_color = node->red;
-    if (*delete_value < node->value)
-      sibling->right->red ? LeftRotate(node) : RLRotate(node);
+    Node*& update_node = *safe_node;
+    bool old_node_color = update_node->red;
+    if (*delete_value < update_node->value)
+      sibling->right->red ? LeftRotate(update_node) : RLRotate(update_node);
     else
-      sibling->left->red ? RightRotate(node) : LRRotate(node);
-    node->red = old_node_color;
-    node->left->red = node->right->red = false;
+      sibling->left->red ? RightRotate(update_node) : LRRotate(update_node);
+    update_node->red = old_node_color;
+    update_node->left->red = update_node->right->red = false;
     return true;
   }
 
@@ -451,6 +346,124 @@ struct RBTree {
       (*parent)->left->red = (*parent)->right->red = false;
       return done = true;
     }
+  }
+
+  static bool TopDownInsert(Node*& node, const T& x) {
+    if (node == sentinel) {
+      node = new Node(x, false);
+      return true;
+    }
+    if (x == node->value) return false;
+    for (Node *nil = &dummy, **gg_parent = &nil, **g_parent = &nil,
+              **parent = &node;
+         ;) {
+      // Loop invariant: the sibling of parent (pointed by g_parent) is black.
+      // current is the node points by the parent, but after the rotation,
+      // parent might point to other node. But the search path is determined by
+      // the current.
+      Node* current = *parent;
+      if (current == sentinel)
+        current = *parent = new Node(x);
+      else if (x == current->value)
+        return node->red = false;
+      else if (current->left->red && current->right->red) {
+        // Flip color.
+        current->red = true;
+        current->left->red = current->right->red = false;
+      }
+      // When two red nodes are connected.
+      if (current->red && (*g_parent)->red) {
+        (*gg_parent)->red = true;
+        if (x < (*gg_parent)->value)
+          x > (*g_parent)->value ? LRRotate(*gg_parent)
+                                 : RightRotate(*gg_parent);
+        else
+          x < (*g_parent)->value ? RLRotate(*gg_parent)
+                                 : LeftRotate(*gg_parent);
+        (*gg_parent)->red = false;
+        // When rotation occurs, rotation cannot occurs in the next iteration.
+        // 	 After double rotation, since current becomes black, red nodes
+        // 	 violation won't occur in the next iteration.
+        // 	 After single rotation, although current is red, since the
+        // children
+        // 	 and grand children must be black, red nodes violation won't
+        // occur
+        // 	 in the next iteration.
+        // It is safe to set parent to be nil, since when double rotation
+        // occurs, we loss the track of g_parent and gg_parent.
+        parent = &nil;
+      }
+      if (x == current->value) break;
+      gg_parent = g_parent;
+      g_parent = parent;
+      parent = x < current->value ? &current->left : &current->right;
+    }
+    node->red = false;
+    return true;
+  }
+
+  static bool TopDownDelete(Node*& node, const T& x) {
+    if (node == sentinel) return false;
+    // Set sibling and g_parent to nil pointer to simplify the initial
+    // condition.
+    Node *to_be_deleted = sentinel, nil, *sibling = &nil, **g_parent = &sibling;
+    for (Node **parent = &node, **next, **next_sibling; *parent != sentinel;
+         g_parent = parent, parent = next, sibling = *next_sibling) {
+      // Loop invariant: when current is black, the sibling is black, and
+      // the parent node (pointed by g_parent) is red.
+      Node*& current = *parent;
+      if (x == current->value) to_be_deleted = current;
+      if (x < current->value) {
+        next = &current->left;
+        next_sibling = &current->right;
+      } else {
+        next = &current->right;
+        next_sibling = &current->left;
+      }
+      // Rebalance is required only when the current and the next node are
+      // black.
+      if (current->red || (*next)->red) continue;
+      // When next sibling is red, rotate.
+      if ((*next_sibling)->red) {
+        current->red = true;
+        (*next_sibling)->red = false;
+        if (x < current->value) {
+          LeftRotate(*parent);
+          parent = &(*parent)->left;
+        } else {
+          RightRotate(*parent);
+          parent = &(*parent)->right;
+        }
+        continue;
+      }
+      // Current has two black children.
+      // If sibling has two black children, flip the color.
+      if (!sibling->right->red && !sibling->left->red) {
+        (*g_parent)->red = false;
+        current->red = sibling->red = true;
+        continue;
+      }
+      // If sibling has one red child, rebalance base on whether the
+      // current node is left or right of its parent.
+      if (*parent == (*g_parent)->left) {
+        sibling->right->red ? LeftRotate(*g_parent) : RLRotate(*g_parent);
+        parent = &(*g_parent)->left->left;
+      } else {
+        sibling->left->red ? RightRotate(*g_parent) : LRRotate(*g_parent);
+        parent = &(*g_parent)->right->right;
+      }
+      current->red = (*g_parent)->red = true;
+      (*g_parent)->left->red = (*g_parent)->right->red = false;
+    }
+    if (to_be_deleted == sentinel) return node->red = false;
+    // When the loop terminates, (*g_parent)->value is the successor of x.
+    to_be_deleted->value = (*g_parent)->value;
+    to_be_deleted = *g_parent;
+    *g_parent =
+        (*g_parent)->left == sentinel ? (*g_parent)->right : (*g_parent)->left;
+    delete to_be_deleted;
+    node->red = false;
+    return true;
   }
 
   // Left rotation:
